d3707f57c5e838b4bf75d7ce1d5a7b1bb550b8e3
zhangchengzheng91 <zhangchengzheng91@163.com>
2025-07-24 21:22:52 +0800

    feat: init vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
